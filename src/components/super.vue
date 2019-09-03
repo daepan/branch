6057eb86120a21974bfc238129bdccfa4862aa4e
murpan <qw04011@gmail.com>
2019-09-04 00:17:11 +0900

    Main page update

--- src/components/super.vue
+++ src/components/super.vue
@@ -1,5 +1,20 @@
 <template>
-    
+    <div class="base__component">
+        <div class="block__box">
+            <p>동전산거</p>
+            <img src="http://file3.instiz.net/data/file3/2018/06/17/6/d/4/6d47629a053eed8f8eb3215deb005b14.gif" alt="">
+        </div>
+        <div class="block__box">
+            <p>default</p>
+            <img src="https://techcrunch.com/wp-content/uploads/2018/01/giphy1.gif?w=730&crop=1" alt="">
+        </div>
+        <div class="block__box">
+            <p>커엽</p>
+            <img src="https://t1.daumcdn.net/cfile/tistory/221C69425704B63E2F" alt="">
+        </div>
+
+
+    </div>
 </template>
 
 <script>
@@ -9,5 +24,12 @@
 </script>
 
 <style scoped>
+    .base__component {
 
+    }
+    .block__box{
+        border: 1px black solid;
+        padding: 30px;
+        margin: 50px;
+    }
 </style>
